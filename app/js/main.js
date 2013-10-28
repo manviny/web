31c99ec15474712fee684e215a779d2806945cd7
manviny <manol@mac.local>
2013-10-28 12:17:40 +0100

    set home page, link to js and css

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,16 @@
 
 require.config({
 	paths : {
-		"angular" : "lib/angular/angular"
+		"jquery.min" : "jquery-1.8.0.min",
+		"angular" : "lib/angular/angular",
+		"jquery.easing" : "jquery.easing.1.3",
+		"bootstrap.min" : "bootstrap.min",
+		"spinner.js" : "spinner.js",
+		"jquery.mobile.customized.min" : "jquery.mobile.customized.min",
+		"modernizr.min" : "vendor/modernizr-2.6.1.min",
+		"loadanim" : "loadanim",
+		"mainmetro" : "mainmetro",
+		"add2home" : "add2home"
 	},
 	shim : {
 		angular  :{
