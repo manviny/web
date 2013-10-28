421c9e23cebb43c037a8a8b56e0e2fb579d1668a
manviny <manol@mac.local>
2013-10-28 10:52:26 +0100

    created first call to external json server

--- app/js/Controllers/controllers.js
+++ app/js/Controllers/controllers.js
@@ -3,4 +3,14 @@
 function MainCtrl($scope) {
 
 	$scope.world = "World";
+
+	// fetch data from server
+	$http({	
+			method: 'POST', 
+			url: 'http://yourUrl/some.json'
+		}).success(function(data)
+	{
+		$scope.items = data.matches; // response data 
+	});
+
 }
